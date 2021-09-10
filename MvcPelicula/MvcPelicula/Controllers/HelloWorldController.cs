cc13490c22a771af5d1ca29785fd7c9c5cfd4e76
DiegoPonce09 <ponce_diego09@hotmail.com>
2021-09-10 13:04:08 -0600

    Guia_DSE git commit -m Guia2_DSE

---
+++ MvcPelicula/MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //Get HelloWorld/Welcome
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            ViewBag.Mensaje = "Hola " + nombre + " " + apellido + ", veces mostrado: " + numVeces;
+            ViewBag.NumVeces = numVeces;
+            return View();
+        }
+    }
+}
---
+++ MvcPersona/MvcPersona/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcPersona.Models
+{
+    public class Persona
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+    public class PersonaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
